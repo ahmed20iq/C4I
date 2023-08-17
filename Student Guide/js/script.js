db760a9c9eccfbef7e63d850ddd3375ab77e7b91
ahmed20iq <60705231+ahmed20iq@users.noreply.github.com>
2023-08-17 19:58:24 +0300

    Delete Student Guide directory

--- Student Guide/js/script.js
+++
@@ -1,6 +0,0 @@
-let slide_btn = document.querySelector('.btn-slide');
-let list = document.querySelector('.navbar ul');
-
-slide_btn.addEventListener('click', ()=>{
-    list.classList.toggle('active')
-})
